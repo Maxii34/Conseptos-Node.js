e6ab6f31d2e01ef8d030f474858fbb281cfc015a
Maxi Ordoñez <exemaxi@outlook.com>
2025-10-01 10:26:32 -0300

    Ejemplo de como usar commonjs

---
+++ src/02-calculadoras.js
@@ -0,0 +1,12 @@
+const suma = (a, b) => {
+    return a + b;
+}
+
+const resta = (a, b) => {
+    return a - b;
+}
+
+module.exports = {
+    suma,  // <- es igual que suma: suma 
+    resta // <- es igual que resta: resta
+}
