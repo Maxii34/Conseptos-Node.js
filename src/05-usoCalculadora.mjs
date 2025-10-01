f8504b6dcca577c35b55cbf38bcf48d811dc6f70
Maxi Ordoñez <exemaxi@outlook.com>
2025-10-01 10:37:55 -0300

    Ejemplo de como usar moduleES

---
+++ src/05-usoCalculadora.mjs
@@ -0,0 +1,4 @@
+import { division, multiplicacion } from "./04-calculadoraExtendida.mjs";
+
+console.log(`Multiplicacion de dos numeros: ${multiplicacion(2, 8)}`)
+console.log(`division de dos numeros: ${division(34, 2)}`)
